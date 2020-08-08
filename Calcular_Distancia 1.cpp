eb3a156b61c40f435d00231af552f0517aa5568f
Eduardo <eduardo>
2020-08-08 18:26:51 -0500

    version: simple

---
+++ Calcular_Distancia 1.cpp
@@ -0,0 +1,21 @@
+
+//ENTREGABLE 4
+
+#include <conio.h>
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float d,v,t;
+    
+    printf("Calcular la distancia de un vehículo a velocidad constante en un tiempo t.");
+    printf("\n\n Ingrese el valor de la velocidad (km/h): "); scanf("%f",&v);
+    printf("\n Ingrese el valor del tiempo (h): "); scanf("%f",&t);
+    
+    d=v*t;
+    
+    printf("\n\n La distancia es: %f",d);
+    getch();
+}    
+    
